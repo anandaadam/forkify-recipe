5892e7c0a90d718c57a967eab176ed7a57d36d09
adamanandas <adamananda02@gmail.com>
2022-10-16 20:31:57 +0700

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4fd1cb45-b6ac-493b-8947-a54c8ff1c214';
+export const MODAL_CLOSE_SEC = 2.5;
